154b63e2bf33915cc2a4d47788d7b29769c595b8
Douglas Lima <douglas.lima.queiroz@gmail.com>
2023-11-22 12:04:17 -0300

    feat- base-html-css-get date

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.getElementById('calendar').textContent = getCurrentDate();
+function getCurrentDate() {
+    const today = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return today.toLocaleDateString('pt-BR', options);
+}
+
